33630433aaf493010b893f6bd919a6e329ef133b
Dave Kjell Marong <davem@stacc.com>
2024-01-31 21:36:28 +0100

    Refactor IntroCarousel component

--- src/app/min-side/selge/last-opp/IntroCarousel.tsx
+++ src/app/min-side/selge/last-opp/IntroCarousel.tsx
@@ -1,60 +1,113 @@
 import React from "react";
 import CarouselComponent from "@/components/carousel/Carousel";
 import Image from "next/image";
-import velgBilder from "../../../../../public/velg-bilder.png";
-import cameraScreenshot from "../../../../../public/camera-screenshot.png";
-import galleryClothes from "../../../../../public/gallery-clothes.png";
-import knappBilder from "../../../../../public/knapp-bilder.png";
 const IntroCarousel = () => {
-  const instructions = [
-    {
-      title: "Steg 1",
-      text: "Gå ut av nettsiden, legg klærne på en flat overflate og ta bilder av dem",
-      img1: cameraScreenshot,
-      img2: galleryClothes,
-    },
-    {
-      title: "Steg 2",
-      text: "Når du har tatt bildene, gå tilbake til nettsiden og trykk på knappen under",
-      img1: knappBilder,
-      img2: velgBilder,
-    },
-  ];
+  const instruction1 = {
+    title: "Steg 1",
+    text: "Gå ut av nettsiden, legg klærne på en flat overflate og ta bilder av dem",
+  };
+
+  const instruction2 = {
+    title: "Steg 2",
+    text: "Når du har tatt bildene, gå tilbake til nettsiden og trykk på knappen under",
+  };
+
   return (
     <CarouselComponent>
-      {instructions.map((instruction) => {
-        return (
-          <div
-            key={instruction.title}
-            className="flex flex-col gap-6 text-center justify-center items-center mx-2 h-[500px]"
-          >
-            <p className="font-light text-sm text-purple-700">
-              {instruction.title}
-            </p>
-            <p className=" mx-4">{instruction.text}</p>
-            <div className="flex gap-8 w-full max-h-[350px]">
-              <Image
-                src={instruction.img1}
-                //   height={100}
-                //   width={100}
-                alt=""
-                className="w-1/2"
-                placeholder="blur"
-              />
-              <Image
-                src={instruction.img2}
-                //   height={100}
-                //   width={100}
-                alt=""
-                className="w-1/2"
-                placeholder="blur"
-              />
-            </div>
-          </div>
-        );
-      })}
+      <div
+        key={instruction1.title}
+        className="flex flex-col gap-6 text-center justify-center items-center mx-2 h-[500px]"
+      >
+        <p className="font-light text-sm text-purple-700">
+          {instruction1.title}
+        </p>
+        <p className=" mx-4">{instruction1.text}</p>
+        <div className="flex gap-8 w-full max-h-[350px]">
+          <Image
+            src="/camera-screenshot.png"
+            height={100}
+            width={100}
+            alt=""
+            className="w-1/2"
+            // placeholder="blur"
+          />
+          <Image
+            src="/gallery-clothes.png"
+            height={100}
+            width={100}
+            alt=""
+            className="w-1/2"
+            // placeholder="blur"
+          />
+        </div>
+      </div>
+
+      <div
+        key={instruction2.title}
+        className="flex flex-col gap-6 text-center justify-center items-center mx-2 h-[500px]"
+      >
+        <p className="font-light text-sm text-purple-700">
+          {instruction2.title}
+        </p>
+        <p className=" mx-4">{instruction2.text}</p>
+        <div className="flex gap-8 w-full max-h-[350px]">
+          <Image
+            src="/knapp-bilder.png"
+            height={100}
+            width={100}
+            alt=""
+            className="w-1/2"
+            // placeholder="blur"
+          />
+          <Image
+            src="/velg-bilder.png"
+            height={100}
+            width={100}
+            alt=""
+            className="w-1/2"
+            // placeholder="blur"
+          />
+        </div>
+      </div>
     </CarouselComponent>
   );
 };
 
 export default IntroCarousel;
+
+//       {instructions.map((instruction) => {
+//         return (
+//           <div
+//             key={instruction.title}
+//             className="flex flex-col gap-6 text-center justify-center items-center mx-2 h-[500px]"
+//           >
+//             <p className="font-light text-sm text-purple-700">
+//               {instruction.title}
+//             </p>
+//             <p className=" mx-4">{instruction.text}</p>
+//             <div className="flex gap-8 w-full max-h-[350px]">
+//               <Image
+//                 src={instruction.img1}
+//   height={100}
+//   width={100}
+//                 alt=""
+//                 className="w-1/2"
+// placeholder = "blur";
+//               />
+//               <Image
+//                 src={instruction.img2}
+//   height={100}
+//   width={100}
+//                 alt=""
+//                 className="w-1/2"
+// placeholder = "blur";xs
+//               />
+//             </div>
+//           </div>
+//         );
+//       })}
+//     </CarouselComponent>
+//   );
+// };
+
+// export default IntroCarousel;
