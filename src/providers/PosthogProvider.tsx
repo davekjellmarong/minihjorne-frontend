5b854213ff0f4aef5dc1691a529cb7e74e8b2d48
Dave Kjell Marong <davem@stacc.com>
2024-06-14 23:05:03 +0200

    chore: Update npm dependencies and enable page leave tracking in PosthogProvider

--- src/providers/PosthogProvider.tsx
+++ src/providers/PosthogProvider.tsx
@@ -7,7 +7,8 @@ if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: "identified_only",
-    capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+    capture_pageview: false,
+    capture_pageleave: true,
   });
 }
 
