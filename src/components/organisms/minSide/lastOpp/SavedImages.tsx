c1319137431128e3e07f1ccb48630fd4b2867fa4
Dave Kjell Marong <davem@stacc.com>
2024-07-07 12:11:08 +0200

    Refactor MinSide component and remove unused code

--- src/components/organisms/minSide/lastOpp/SavedImages.tsx
+++ src/components/organisms/minSide/lastOpp/SavedImages.tsx
@@ -8,8 +8,8 @@ interface SavedImagesProps {
   setModal: any;
 }
 const SavedImages = ({ images, setImages, setModal }: SavedImagesProps) => {
-  if (!images) return null;
   const unusedImages = images.filter((image) => !image.caption);
+  if (unusedImages.length == 0) return null;
   return (
     <div className="bg-brand-100 p-4">
       <div className="flex justify-center gap-2 pb-4">
@@ -21,14 +21,6 @@ const SavedImages = ({ images, setImages, setModal }: SavedImagesProps) => {
         >
           Fortsett med {unusedImages.length} lagrede bilder
         </Button>
-        {/* <Button
-          type="outline"
-          onClick={() => {
-            setPreviousImages(false);
-          }}
-        >
-          Last opp flere bilder
-        </Button> */}
       </div>
       <ul className="flex max-h-[400px] flex-wrap justify-center gap-8 overflow-scroll">
         {unusedImages.map((image) => (
