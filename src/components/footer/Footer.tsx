f71bb3d7ce789b3ea400ac8c4df227e242c56450
Dave Kjell Marong <davem@stacc.com>
2024-07-14 23:12:59 +0200

    feat: Add Instagram link to footer
    - Import Image and Link components
    - Add Instagram link with logo and username to footer
    
    Closes #456

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
     <footer className="py-8">
       <hr />
-      <div className=" mx-auto flex flex-col gap-2 py-6 text-center">
-        <p className="text-gray-600">Minihjørne</p>
-        <p className="text-xs text-gray-400">Marong Utvikling</p>
+      <div className=" mx-auto flex flex-col items-center gap-2 py-6 text-center">
+        {/* <p className="text-gray-600">Minihjørne</p> */}
+        <Link
+          href="https://instagram.com/mini_hjorne"
+          className="flex items-center gap-2"
+        >
+          <Image src="/instagram.svg" alt="logo" width={50} height={50} />
+          <p>mini_hjorne</p>
+        </Link>
+        {/* <p className="text-xs text-gray-400">Marong Utvikling</p> */}
       </div>
     </footer>
   );
