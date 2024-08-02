0de530fbdce942089a56fc63314958c2991946c0
Dave Kjell Marong <davem@stacc.com>
2024-08-02 20:04:13 +0200

    Add personal profiles to posthog events

--- src/providers/PosthogProvider.tsx
+++ src/providers/PosthogProvider.tsx
@@ -7,7 +7,7 @@ if (typeof window !== "undefined") {
   if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-      person_profiles: "identified_only",
+      person_profiles: "always",
       capture_pageview: true,
       capture_pageleave: true,
     });
