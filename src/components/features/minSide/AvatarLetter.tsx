9cfcbb92dfcd52495d3ebc460467b8a1db4cf432
Dave Kjell Marong <davem@stacc.com>
2024-08-04 14:22:50 +0200

    chore: Update AvatarLetter component to display admin status
    The AvatarLetter component has been updated to include an additional prop `admin` which determines whether the user is an admin. If the user is an admin, a green border is added to the avatar. This change improves the visual representation of admin users in the MinSide page.

--- src/components/features/minSide/AvatarLetter.tsx
+++ src/components/features/minSide/AvatarLetter.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 
-const AvatarLetter = ({ username }: { username: string }) => {
+const AvatarLetter = ({
+  username,
+  admin,
+}: {
+  username: string;
+  admin: boolean;
+}) => {
   return (
-    <span className="rounded-full border bg-white px-8 py-6 text-center text-5xl shadow-sm">
+    <span
+      className={`rounded-full border ${admin ? "border-2 border-green-500" : null} bg-white px-8 py-6 text-center text-5xl shadow-sm`}
+    >
       {username?.slice(0, 1).toUpperCase()}
     </span>
   );
