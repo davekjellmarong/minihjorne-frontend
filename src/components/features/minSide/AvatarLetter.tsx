71b394809e6202426fc17c14d4aba03c00125cf7
Dave Kjell Marong <davem@stacc.com>
2024-08-04 14:57:35 +0200

    Update AvatarLetter component to fix border color issue
    The AvatarLetter component has been updated to fix an issue with the border color when the user is an admin. The border color was not being applied correctly due to a typo in the class name. This change ensures that the green border is displayed correctly for admin users in the MinSide page.

--- src/components/features/minSide/AvatarLetter.tsx
+++ src/components/features/minSide/AvatarLetter.tsx
@@ -9,7 +9,7 @@ const AvatarLetter = ({
 }) => {
   return (
     <span
-      className={`rounded-full border ${admin ? "border-2 border-green-500" : null} bg-white px-8 py-6 text-center text-5xl shadow-sm`}
+      className={`rounded-full border ${admin ? "border border-green-500" : null} bg-white px-8 py-6 text-center text-5xl shadow-sm`}
     >
       {username?.slice(0, 1).toUpperCase()}
     </span>
