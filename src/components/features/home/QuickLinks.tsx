e6e48182755a9dbaad3f393fe7b94d0344ce7d4d
Dave Kjell Marong <davem@stacc.com>
2024-08-17 01:07:36 +0200

    chore: Update QuickLinks component links
    The code changes in `QuickLinks.tsx` update the links in the QuickLinks component to remove the pagination parameter. This change ensures that the links navigate to the correct pages without unnecessary pagination, improving the user experience.

--- src/components/features/home/QuickLinks.tsx
+++ src/components/features/home/QuickLinks.tsx
@@ -7,49 +7,49 @@ const QuickLinks = () => {
   const quickFilterData = [
     {
       name: "Gutt",
-      link: "/produkter?&filters[sex][id][$eq]=1&pagination[page]=1",
+      link: "/produkter?&filters[sex][id][$eq]=1",
       image:
         "https://res.cloudinary.com/dylzaicv5/image/upload/v1721369331/StaticAssets/quicklink-gutt_njuiva.jpg",
     },
     {
       name: "Jente",
-      link: "/produkter?&filters[sex][id][$eq]=2&pagination[page]=1",
+      link: "/produkter?&filters[sex][id][$eq]=2",
       image:
         "https://res.cloudinary.com/dylzaicv5/image/upload/v1721369327/StaticAssets/quicklink-jente_cnnn6i.jpg",
     },
     {
       name: "Unisex",
-      link: "/produkter?&filters[sex][id][$eq]=3&pagination[page]=1",
+      link: "/produkter?&filters[sex][id][$eq]=3",
       image:
         "https://res.cloudinary.com/dylzaicv5/image/upload/v1721369326/StaticAssets/quicklink-unisex_ksqzh1.jpg",
     },
     {
       name: "Nyfødt",
-      link: "/produkter?&filters[size][id][$eq]=21&pagination[page]=1",
+      link: "/produkter?&filters[size][id][$eq]=21",
       image:
         "https://res.cloudinary.com/dylzaicv5/image/upload/v1721369328/StaticAssets/quicklink-nyfodt_f1cxos.jpg",
     },
     {
       name: "Body",
-      link: "/produkter?&filters[category][id][$eq]=2&pagination[page]=1",
+      link: "/produkter?&filters[category][id][$eq]=2",
       image:
         "https://res.cloudinary.com/dylzaicv5/image/upload/v1721369328/StaticAssets/quicklink-body_lk6z2n.jpg",
     },
     {
       name: "Kjole",
-      link: "/produkter?&filters[category][id][$eq]=4&pagination[page]=1",
+      link: "/produkter?&filters[category][id][$eq]=4",
       image:
         "https://res.cloudinary.com/dylzaicv5/image/upload/v1721369326/StaticAssets/quicklink-kjole_i3c20q.jpg",
     },
     // {
     //   name: "Sko",
-    //   link: "/produkter?&filters[category][id][$eq]=7&pagination[page]=1",
+    //   link: "/produkter?&filters[category][id][$eq]=7",
     //   image:
     //     "https://res.cloudinary.com/dylzaicv5/image/upload/v1721369325/StaticAssets/quicklink-sko_iolpap.jpg",
     // },
     {
       name: "Yttertøy",
-      link: "/produkter?&filters[category][id][$eq]=7&filters[category][id][$eq]=14&filters[category][id][$eq]=15&pagination[page]=1",
+      link: "/produkter?&filters[category][id][$eq]=7&filters[category][id][$eq]=14&filters[category][id][$eq]=15",
       image:
         "https://res.cloudinary.com/dylzaicv5/image/upload/v1721369325/StaticAssets/quicklink-yttertoy_ynhh0u.jpg",
     },
