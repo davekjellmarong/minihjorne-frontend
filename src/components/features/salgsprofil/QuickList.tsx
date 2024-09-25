bb302510ff5c7bd2b2bc7deed4ebd6c248c118c8
Dave Kjell Marong <davem@stacc.com>
2024-09-25 18:19:41 +0200

    Posthog custom events for filter presses and homepage layout update

--- src/components/features/salgsprofil/QuickList.tsx
+++ src/components/features/salgsprofil/QuickList.tsx
@@ -5,7 +5,7 @@ import Product from "./Product";
 const QuickList = async () => {
   const sellers = await SellerMethods.getAll();
   return (
-    <section className="w-full bg-white py-12">
+    <section className="w-full bg-white">
       <div className="grid gap-6">
         <div className="flex flex-col items-start gap-4 md:flex-row md:items-center md:gap-8">
           <div className="grid gap-1 px-2">
@@ -18,6 +18,51 @@ const QuickList = async () => {
           </div>
         </div>
         <div className="grid gap-8 lg:grid-cols-2">
+          {sellers?.slice(0, 6).map((profile) => (
+            <div key={profile.id} className="group relative grid gap-4">
+              <div className="grid gap-2 rounded-lg border border-gray-200 bg-white p-4">
+                <QuickLink seller={{ data: profile }} />
+                <div className="grid grid-cols-2 gap-4">
+                  {/* Sort by randomWeight */}
+                  {profile.attributes.products?.data
+                    ?.slice(0, 2)
+                    .map((product) => (
+                      <Product key={product.id} product={product} />
+                    ))}
+                </div>
+              </div>
+            </div>
+          ))}
+          {sellers?.slice(0, 6).map((profile) => (
+            <div key={profile.id} className="group relative grid gap-4">
+              <div className="grid gap-2 rounded-lg border border-gray-200 bg-white p-4">
+                <QuickLink seller={{ data: profile }} />
+                <div className="grid grid-cols-2 gap-4">
+                  {/* Sort by randomWeight */}
+                  {profile.attributes.products?.data
+                    ?.slice(0, 2)
+                    .map((product) => (
+                      <Product key={product.id} product={product} />
+                    ))}
+                </div>
+              </div>
+            </div>
+          ))}
+          {sellers?.slice(0, 6).map((profile) => (
+            <div key={profile.id} className="group relative grid gap-4">
+              <div className="grid gap-2 rounded-lg border border-gray-200 bg-white p-4">
+                <QuickLink seller={{ data: profile }} />
+                <div className="grid grid-cols-2 gap-4">
+                  {/* Sort by randomWeight */}
+                  {profile.attributes.products?.data
+                    ?.slice(0, 2)
+                    .map((product) => (
+                      <Product key={product.id} product={product} />
+                    ))}
+                </div>
+              </div>
+            </div>
+          ))}
           {sellers?.slice(0, 6).map((profile) => (
             <div key={profile.id} className="group relative grid gap-4">
               <div className="grid gap-2 rounded-lg border border-gray-200 bg-white p-4">
